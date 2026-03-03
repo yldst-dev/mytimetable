0d0c02d697863342f22ca03d933cd009f5a7edd6
yldst-dev <185788074+yldst-dev@users.noreply.github.com>
2026-03-03 18:11:29 +0900

    feat: apply global favicon and apple touch icon

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,10 +6,6 @@
 
 <svelte:head>
 	<title>주간 강의 시간표</title>
-	<link
-		rel="icon"
-		href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>⏰</text></svg>"
-	/>
 </svelte:head>
 
 <div class="flex h-[100dvh] flex-col overflow-hidden bg-gray-100 py-2 md:min-h-screen md:py-6">
